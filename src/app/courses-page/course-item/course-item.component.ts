e0c48b7130b58ad60b601256f89d3a3fc48e7241
Anastasiia Grigorenko <Anastasia.Grigorenko@externals.adidas.com>
2020-04-26 22:24:31 +0300

    added project modules, components, interfaces

---
+++ src/app/courses-page/course-item/course-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-item',
+  templateUrl: './course-item.component.html',
+  styleUrls: ['./course-item.component.sass']
+})
+export class CourseItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
