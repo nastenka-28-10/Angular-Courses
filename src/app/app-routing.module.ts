979d0725296bfdd9b7838a426a950df5144fd4b6
Anastasiia Grigorenko <Anastasia.Grigorenko@externals.adidas.com>
2020-05-04 19:48:44 +0300

    task2: added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursesContainerComponent } from 'app/modules/courses-page/courses-container/courses-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'courses', component: CoursesContainerComponent },
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
