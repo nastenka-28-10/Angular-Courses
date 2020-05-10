e6e25c354ba35c2af13751279815595e22be5c01
Anastasiia Grigorenko <Anastasia.Grigorenko@externals.adidas.com>
2020-05-10 22:00:17 +0300

    task4: created courses-data-service, implemented delete action

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DurationPipe } from 'app/modules/core/duration-pipe/duration.pipe';
+import { ConfirmModalComponent } from 'app/modules/core/confirm-modal/confirm-modal.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, DurationPipe],
+  declarations: [HeaderComponent, FooterComponent, DurationPipe, ConfirmModalComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, FooterComponent, DurationPipe],
+  exports: [HeaderComponent, FooterComponent, DurationPipe, ConfirmModalComponent],
 })
 export class CoreModule {}
---
+++ src/app/modules/courses-page/mock-courses.ts
@@ -0,0 +1,88 @@
+import { CourseItem } from 'app/interfaces/course-item';
+
+export const COURSES: CourseItem[] = [
+  {
+    id: 1,
+    title: 'React',
+    creationDate: new Date('11/08/2020').toString(),
+    durationMinutes: 60,
+    description:
+      'Learn about where you can find course descriptions,\n' +
+      '        what information they include, how they work, and details about various components\n' +
+      '        of a course description. Course descriptions report information about a university\n' +
+      "        or college's classes. They're published both in course catalogs that outline degree\n" +
+      '        requirements and in course schedules that contain descriptions for all courses offered\n' +
+      '        during a particular semester',
+    topRated: true,
+  },
+  {
+    id: 2,
+    title: 'React Native',
+    creationDate: new Date('05/01/2020').toString(),
+    durationMinutes: 80,
+    description:
+      'Learn about where you can find course descriptions,\n' +
+      '        what information they include, how they work, and details about various components\n' +
+      '        of a course description. Course descriptions report information about a university\n' +
+      "        or college's classes. They're published both in course catalogs that outline degree\n" +
+      '        requirements and in course schedules that contain descriptions for all courses offered\n' +
+      '        during a particular semester',
+    topRated: false,
+  },
+  {
+    id: 3,
+    title: 'AngularJS',
+    creationDate: new Date('04/29/2019').toString(),
+    durationMinutes: 295,
+    description:
+      'Learn about where you can find course descriptions,\n' +
+      '        what information they include, how they work, and details about various components\n' +
+      '        of a course description. Course descriptions report information about a university\n' +
+      "        or college's classes. They're published both in course catalogs that outline degree\n" +
+      '        requirements and in course schedules that contain descriptions for all courses offered\n' +
+      '        during a particular semester',
+    topRated: false,
+  },
+  {
+    id: 4,
+    title: 'JQuery',
+    creationDate: new Date('12/05/2017').toString(),
+    durationMinutes: 45,
+    description:
+      'Learn about where you can find course descriptions,\n' +
+      '        what information they include, how they work, and details about various components\n' +
+      '        of a course description. Course descriptions report information about a university\n' +
+      "        or college's classes. They're published both in course catalogs that outline degree\n" +
+      '        requirements and in course schedules that contain descriptions for all courses offered\n' +
+      '        during a particular semester',
+    topRated: true,
+  },
+  {
+    id: 5,
+    title: 'Angular',
+    creationDate: new Date('05/25/2020').toString(),
+    durationMinutes: 120,
+    description:
+      'Learn about where you can find course descriptions,\n' +
+      '        what information they include, how they work, and details about various components\n' +
+      '        of a course description. Course descriptions report information about a university\n' +
+      "        or college's classes. They're published both in course catalogs that outline degree\n" +
+      '        requirements and in course schedules that contain descriptions for all courses offered\n' +
+      '        during a particular semester',
+    topRated: true,
+  },
+  {
+    id: 6,
+    title: 'Vue',
+    creationDate: new Date('05/06/2020').toString(),
+    durationMinutes: 60,
+    description:
+      'Learn about where you can find course descriptions,\n' +
+      '        what information they include, how they work, and details about various components\n' +
+      '        of a course description. Course descriptions report information about a university\n' +
+      "        or college's classes. They're published both in course catalogs that outline degree\n" +
+      '        requirements and in course schedules that contain descriptions for all courses offered\n' +
+      '        during a particular semester',
+    topRated: false,
+  },
+];
