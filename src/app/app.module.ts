90824bb07dafb598eed5107c96ab91cd1008f445
Anastasiia Grigorenko <Anastasia.Grigorenko@externals.adidas.com>
2020-05-24 22:19:38 +0300

    task6: implemented interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
 import { CoursesPageModule } from './modules/courses-page/courses-page.module';
 import { LoginPageModule } from 'app/modules/login-page/login-page.module';
+import {AuthTokenInterceptor} from 'app/modules/core/auth-token.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +19,13 @@ import { LoginPageModule } from 'app/modules/login-page/login-page.module';
     CoursesPageModule,
     LoginPageModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthTokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
