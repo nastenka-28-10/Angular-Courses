46237431146977869b805e2910202d7de404201f
Anastasiia Grigorenko <Anastasia.Grigorenko@externals.adidas.com>
2020-06-07 16:44:55 +0300

    task7: set rxJS for courses search

--- src/app/modules/courses-page/search-course/search-course.component.ts
+++ src/app/modules/courses-page/search-course/search-course.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Subject} from 'rxjs';
+import { map, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-course',
@@ -6,14 +8,16 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./search-course.component.scss'],
 })
 export class SearchCourseComponent implements OnInit {
+  inputChange: Subject<HTMLInputElement> = new Subject();
   @Output() searchCourse: EventEmitter<string> = new EventEmitter<string>();
-  courseNameToSearch = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
-
-  onClickSearch(): void {
-    this.searchCourse.emit(this.courseNameToSearch);
+  ngOnInit(): void {
+    this.inputChange.pipe(
+      debounceTime(300),
+      map((target: HTMLInputElement): string => target.value),
+      filter((inputValue: string): boolean => !inputValue || inputValue.length >= 3)
+    ).subscribe((value: string) => this.searchCourse.emit(value));
   }
 }
